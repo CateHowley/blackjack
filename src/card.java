a7bc18900d73c948552fbe9a5fa41ce10d539d03
catehowley <Catherine_howley25@milton.edu>
2023-06-05 14:15:23 -0400

    jack black shuffled deck

--- src/card.java
+++ src/card.java
@@ -4,12 +4,37 @@ public class card {
     public String name;
     public boolean isDealt;
 
-    public card(int pSuit, int pName){
-        if(pSuit == 0){
+
+    public card(int pSuit, int pName) {
+        if (pSuit == 0) {
             suit = "Diamond";
         }
-        if(pSuit == 0){
+        if (pSuit == 1) {
             suit = "spade";
         }
+        if (pSuit == 2) {
+            suit = "heart";
+        }
+        if (pSuit == 3) {
+            suit = "club";
+        }
+        if (pName == 10) {
+            suit = "King";
+
+        }
+        if (pName == 11) {
+            suit = "ace";
+
+        }
     }
+
+    public void print() {
+        System.out.println("your card is" + name + "of" + suit);
+    }
+
 }
+
+
+
+
+
--- src/game.java
+++ src/game.java
@@ -1,8 +1,54 @@
+
 public class game {
+    public int a;
+    public int b;
+    public card card1;
+    public card[] deck;
+
     public static void main(String[] args) {
-        game blackjack = new game();
+        game mygame= new game();
     }
 
 
+    public game() {
+        // System.out.println("Game Constructor");
+        deck = new card[52];
+        for (int w = 0; w < 4; w++) {
+            for (int x = 0; x < 13; x++) {
+                //deck[count] = new card(e, w);
 
-}
+            }
+            // deck[0].print();
+            // card1= new card(1,1);
+            // card1.print();
+        }
+    }
+            public void shuffle() {
+                    card[] shuffleDeck = new card[52];
+
+                    for (int f = 0; f < shuffleDeck.length; f++) {
+
+                        int randomCard = (int) (Math.random() * 52);
+                        card shuffelCard = deck[randomCard];
+                        if(deck[randomCard].isDealt==true){
+                            randomCard = (int) (Math.random() * 52);
+                            shuffelCard.printCard();
+                        }
+                        deck[randomCard].isDealt = true;
+                        shuffelCard.printCard();
+
+                        shuffleDeck[f]=shuffelCard;
+
+
+
+                    }
+                    deck=shuffleDeck;
+
+                }
+            }
+
+
+
+
+
+
